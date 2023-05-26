8d4a657f111feed732ac0a24f6f3c7b9373dde4d
Alina Kuptsova <linakuptsova92@gmail.com>
2023-05-26 18:10:17 +0200

    functions ex9

---
+++ ex9.py
@@ -0,0 +1,10 @@
+def get_last_char(string):
+    return string[-1]
+
+
+print(get_last_char('hexlet'))  # t
+
+print(get_last_char('code'))  # e
+
+text = 'Winter is coming'
+print(get_last_char(text))  # g
