46517296542b78860257a9da7891b26d51f06e7e
Alina Kuptsova <linakuptsova92@gmail.com>
2023-06-05 22:02:58 +0200

    data aggregation (string)

---
+++ ex19.py
@@ -0,0 +1,9 @@
+
+# data aggregation (string)
+def join_numbers_from_range(start, finish):
+    result = ''
+    i = start
+    while i <= finish:
+        result = result + str(i)
+        i = i + 1
+    return result
