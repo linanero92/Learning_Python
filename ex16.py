984744dc4db4e67230395f174414d81cac5699e4
Alina Kuptsova <linakuptsova92@gmail.com>
2023-06-05 19:23:31 +0200

    match operator

---
+++ ex16.py
@@ -0,0 +1,12 @@
+
+# match operator
+def get_number_explanation(number):
+    match number:
+        case 666:
+            return 'devil number'
+        case 42:
+            return 'answer for everything'
+        case 7:
+            return 'prime number'
+        case _:
+            return 'just a number'
